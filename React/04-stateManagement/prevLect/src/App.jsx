6f53d8bae8d9f1078c47f31e27a7d1545071c0db
yogeshftv <yogesh.ftv.ind@gmail.com>
2024-03-27 11:04:42 +0530

    made some changes

--- React/04-stateManagement/prevLect/src/App.jsx
+++ React/04-stateManagement/prevLect/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+  
 // React controls UI Updation & Manipulation using hooks
 
 function App() {
@@ -36,4 +36,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
