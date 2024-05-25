7c4bc46b0ba04b8a3b28249d3dc091eb376500bf
yogeshftv <yogesh.ftv.ind@gmail.com>
2024-05-25 12:39:39 +0530

    first commit for the repo

---
+++ C#/FirstConsoleApp/Program.cs
@@ -0,0 +1,3 @@
+﻿System.Console.Write("Hello C#");
+System.Console.WriteLine("Adds linefeed (newline char) at the end of the output");
+System.Console.WriteLine("`Console.Write()` does not appends linefeed (newline char) at the end of the output");
---
+++ C#/docs/basics.md
@@ -0,0 +1,6 @@
+# Check dotnet version
+`dotnet --version`
+
+# Create Project Setup
+`dotnet new console -n MyConsoleApp1`;
+
